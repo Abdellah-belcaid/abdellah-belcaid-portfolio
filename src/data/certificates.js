758fe4d4ca83250dcca2fb4e5ae999da83d879ed
belcaid <belcaid.abdellah2001@gmail.com>
2023-10-21 16:29:12 +0200

    add new certificates

--- src/data/certificates.js
+++ src/data/certificates.js
@@ -1,84 +1,266 @@
 export const certificationList = [
   {
-    name: "Developing Front-End Apps with React",
-    organization: "React Certification Institute",
-    date: "June 2022",
-    imageURL: "/images/certificates/Developing_Front_End_Apps_with_React.jpg",
-    skillsEarned: ["Frontend Development", "React"],
-    tag: ["All", "Frontend"],
-    viewLink: "",
-  },
-  {
-    name: "Getting Started with Spring Boot",
-    organization: "Spring Boot Academy",
-    date: "January 2023",
-    imageURL: "/images/certificates/Getting_started_with_spring_boot.jpg",
-    skillsEarned: ["Spring Boot"],
-    tag: ["All", "Backend"],
-    viewLink: "",
-  },
-  {
-    name: "Meta - React Basics",
-    organization: "React Certification Institute",
-    date: "August 2022",
-    imageURL: "/images/certificates/Meta_React_Basics.jpg",
-    skillsEarned: ["Frontend Development", "React"],
-    tag: ["All", "Frontend"],
-    viewLink: "",
-  },
-  {
-    name: "Securing Your Spring Boot 3.0 Applications with JWT",
-    organization: "Security Certifications Inc.",
-    date: "April 2023",
-    imageURL:
-      "/images/certificates/securing_your_spring_boot_3-0_applications_with_JWT.jpg",
-    skillsEarned: ["Spring Boot", "Security"],
-    tag: ["All", "Backend", "Security"],
-    viewLink: "",
-  },
-  {
-    name: "SoloLearn - SQL Intermediate Certificate",
-    organization: "SoloLearn",
-    date: "July 2021",
-    imageURL: "/images/certificates/SoloLearn_SQL_Intermediate.jpg",
-    skillsEarned: ["SQL"],
+    name: "SQL Advanced",
+    organization: "Certification Institute",
+    date: "Month Year", // Replace with the actual date
+    imageURL: "/images/certificates/sql_advanced_certificate.png",
+    skillsEarned: ["SQL", "Database Management"],
     tag: ["All", "Database"],
     viewLink: "",
   },
   {
-    name: "Spring Boot S3 File Uploads with React",
+    name: "SQL Basic",
+    organization: "Certification Institute",
+    date: "Month Year", // Replace with the actual date
+    imageURL: "/images/certificates/sql_basic_certificate.png",
+    skillsEarned: ["SQL", "Database Fundamentals"],
+    tag: ["All", "Database"],
+    viewLink: "",
+  },
+  {
+    name: "SQL Intermediate",
+    organization: "Certification Institute",
+    date: "Month Year", // Replace with the actual date
+    imageURL: "/images/certificates/sql_intermediate_certificate.png",
+    skillsEarned: ["SQL", "Intermediate Database Management"],
+    tag: ["All", "Database"],
+    viewLink: "",
+  },
+  {
+    name: "Developing Front-End Apps with React",
     organization: "React Certification Institute",
-    date: "May 2022",
-    imageURL: "/images/certificates/Spring_Boot_S3_File_Uploads_with_React.jpg",
-    skillsEarned: ["Frontend Development", "Spring Boot", "React"],
-    tag: ["All", "Frontend", "Backend"],
+    date: "June 2022",
+    imageURL: "/images/certificates/Developing_Front-End_Apps_with_React.png",
+    skillsEarned: ["Frontend Development", "React"],
+    tag: ["All", "Frontend"],
+    viewLink: "",
+  },
+  {
+    name: "Angular 7 for Advanced Level",
+    organization: "Angular Certification Institute",
+    date: "Month Year", // Replace with the actual date
+    imageURL: "/images/certificates/Angular7_for_Advanced_Level.png",
+    skillsEarned: ["Angular", "Advanced Frontend Development"],
+    tag: ["All", "Frontend"],
+    viewLink: "",
+  },
+  {
+    name: "Angular 7 for Intermediate Level",
+    organization: "Angular Certification Institute",
+    date: "Month Year", // Replace with the actual date
+    imageURL: "/images/certificates/Angular7_for_Intermediate_Level.png",
+    skillsEarned: ["Angular", "Intermediate Frontend Development"],
+    tag: ["All", "Frontend"],
+    viewLink: "",
+  },
+  {
+    name: "Git Tutorial",
+    organization: "Great Learning",
+    date: "Month Year", // Replace with the actual date
+    imageURL: "/images/certificates/Great_Learning-Git_Tutorial.png",
+    skillsEarned: ["Version Control", "Git"],
+    tag: ["All", "Version Control"],
+    viewLink: "",
+  },
+  {
+    name: "React JS Tutorial",
+    organization: "Great Learning",
+    date: "Month Year", // Replace with the actual date
+    imageURL: "/images/certificates/Great_Learning-React_JS_Tutorial.png",
+    skillsEarned: ["Frontend Development", "React"],
+    tag: ["All", "Frontend"],
+    viewLink: "",
+  },
+  {
+    name: "Dockerize Spring Boot Application",
+    organization: "Great Learning",
+    date: "Month Year", // Replace with the actual date
+    imageURL:
+      "/images/certificates/GreatLearning-Dockerize_Spring_Boot_Application.png",
+    skillsEarned: ["Containerization", "Docker", "Spring Boot"],
+    tag: ["All", "DevOps"],
+    viewLink: "",
+  },
+  {
+    name: "Introduction to DevOps",
+    organization: "Certification Institute",
+    date: "Month Year", // Replace with the actual date
+    imageURL: "/images/certificates/Introduction_to_DevOps.png",
+    skillsEarned: ["DevOps", "Continuous Integration", "Continuous Deployment"],
+    tag: ["All", "DevOps"],
+    viewLink: "",
+  },
+  {
+    name: "Securing Your Spring Boot 3.0 Applications with JWT",
+    organization: "Certification Institute",
+    date: "Month Year", // Replace with the actual date
+    imageURL:
+      "/images/certificates/certificate_of_completion_for_securing_your_spring_boot_3-0_applications_with_JWT.png",
+    skillsEarned: ["Security", "Spring Boot", "JWT"],
+    tag: ["All", "Security"],
+    viewLink: "",
+  },
+  {
+    name: "Getting Started with Spring Boot",
+    organization: "Certification Institute",
+    date: "Month Year", // Replace with the actual date
+    imageURL:
+      "/images/certificates/certificate-of-completion-for-getting-started-with-spring-boot.png",
+    skillsEarned: ["Spring Boot", "Java"],
+    tag: ["All", "Java"],
     viewLink: "",
   },
   {
     name: "Spring Data MongoDB",
-    organization: "MongoDB Certification",
-    date: "July 2023",
-    imageURL: "/images/certificates/spring_data_mongodb.jpg",
-    skillsEarned: ["MongoDB", "Spring Data"],
-    tag: ["All", "Database", "Backend"],
+    organization: "Certification Institute",
+    date: "Month Year", // Replace with the actual date
+    imageURL:
+      "/images/certificates/certificate-of-completion-for-spring-data-mongodb.png",
+    skillsEarned: ["Spring Data", "MongoDB", "Java"],
+    tag: ["All", "Java"],
     viewLink: "",
   },
   {
-    name: "Java Basic Certificate",
-    organization: "Java Certification Institute",
-    date: "March 2022",
-    imageURL: "/images/certificates/java_basic_certificate.jpg",
-    skillsEarned: ["Java"],
-    tag: ["All", "Programming"],
+    name: "Java Basic",
+    organization: "Certification Institute",
+    date: "Month Year", // Replace with the actual date
+    imageURL: "/images/certificates/java_basic_certificate.png",
+    skillsEarned: ["Java", "Programming Fundamentals"],
+    tag: ["All", "Java"],
+    viewLink: "",
+  },
+
+  {
+    name: "Spring Boot S3 File Uploads with React",
+    organization: "Certification Institute",
+    date: "Month Year", // Replace with the actual date
+    imageURL:
+      "/images/certificates/Spring_Boot_S3_File_Uploads_with_React_certificate.png",
+    skillsEarned: ["Spring Boot", "React", "Amazon S3"],
+    tag: ["All", "Java", "Frontend"],
+    viewLink: "",
+  },
+
+  {
+    name: "React Advanced",
+    organization: "Meta Certification Institute",
+    date: "Month Year", // Replace with the actual date
+    imageURL: "/images/certificates/Meta_-_React_Advanced.png",
+    skillsEarned: ["React", "Frontend Development"],
+    tag: ["All", "Frontend"],
     viewLink: "",
   },
   {
-    name: "Data Structures and Algorithms in Java",
-    organization: "Algorithms Certification",
-    date: "December 2021",
-    imageURL: "/images/certificates/Data_Structures_and_Algorithms_in_Java.jpg",
+    name: "React Basics",
+    organization: "Meta Certification Institute",
+    date: "Month Year", // Replace with the actual date
+    imageURL: "/images/certificates/Meta_-_React_Basics.png",
+    skillsEarned: ["React", "Frontend Development"],
+    tag: ["All", "Frontend"],
+    viewLink: "",
+  },
+  {
+    name: "Angular Firestore Workshop",
+    organization: "Mindluster",
+    date: "Month Year", // Replace with the actual date
+    imageURL:
+      "/images/certificates/Mindluster__-_Angular_FireStore_WorkShop.png",
+    skillsEarned: ["Angular", "Firestore", "Database"],
+    tag: ["All", "Frontend", "Database"],
+    viewLink: "",
+  },
+  {
+    name: "Docker for Developers",
+    organization: "Mindluster",
+    date: "Month Year", // Replace with the actual date
+    imageURL: "/images/certificates/Mindluster_-_Docker_For_Developers.png",
+    skillsEarned: ["Docker", "Containerization"],
+    tag: ["All", "DevOps"],
+    viewLink: "",
+  },
+  {
+    name: "Microservices with Spring Cloud",
+    organization: "Mindluster",
+    date: "Month Year", // Replace with the actual date
+    imageURL:
+      "/images/certificates/Mindluster_-_Microservices_with_Spring_Cloud.png",
+    skillsEarned: ["Microservices", "Spring Cloud"],
+    tag: ["All", "DevOps"],
+    viewLink: "",
+  },
+  {
+    name: "ReactJS Redux Basics",
+    organization: "Mindluster",
+    date: "Month Year", // Replace with the actual date
+    imageURL: "/images/certificates/Mindluster_-_ReactJS_Redux_Basics.png",
+    skillsEarned: ["React", "Redux", "Frontend Development"],
+    tag: ["All", "Frontend"],
+    viewLink: "",
+  },
+  {
+    name: "Angular 2 Spotify App",
+    organization: "Mindluster",
+    date: "Month Year", // Replace with the actual date
+    imageURL: "/images/certificates/Mindluster-Angular_2_Spotify_App.png",
+    skillsEarned: ["Angular", "Frontend Development"],
+    tag: ["All", "Frontend"],
+    viewLink: "",
+  },
+  {
+    name: "Flux React.js",
+    organization: "Mindluster",
+    date: "Month Year", // Replace with the actual date
+    imageURL: "/images/certificates/Mindluster-Flux_React_js.png",
+    skillsEarned: ["React", "Flux", "Frontend Development"],
+    tag: ["All", "Frontend"],
+    viewLink: "",
+  },
+  {
+    name: "Git GitHub",
+    organization: "Mindluster",
+    date: "Month Year", // Replace with the actual date
+    imageURL: "/images/certificates/Mindluster-Git_GitHub.png",
+    skillsEarned: ["Version Control", "Git"],
+    tag: ["All", "Version Control"],
+    viewLink: "",
+  },
+  {
+    name: "Next.js",
+    organization: "Mindluster",
+    date: "Month Year", // Replace with the actual date
+    imageURL: "/images/certificates/Mindluster-Nextjs.png",
+    skillsEarned: ["Next.js", "Frontend Development"],
+    tag: ["All", "Frontend"],
+    viewLink: "",
+  },
+  {
+    name: "Tailwind CSS",
+    organization: "Mindluster",
+    date: "Month Year", // Replace with the actual date
+    imageURL: "/images/certificates/Mindluster-Tailwind_CSS.png",
+    skillsEarned: ["Tailwind CSS", "Frontend Development"],
+    tag: ["All", "Frontend"],
+    viewLink: "",
+  },
+  {
+    name: "SQL Intermediate",
+    organization: "SoloLearn",
+    date: "Month Year", // Replace with the actual date
+    imageURL:
+      "/images/certificates/SoloLearn_-_SQL_Intermediate_Certificate.png",
+    skillsEarned: ["SQL", "Database Management"],
+    tag: ["All", "Database"],
+    viewLink: "",
+  },
+  {
+    name: "Complete Data Structures and Algorithms in Java",
+    organization: "Certification Institute",
+    date: "Month Year", // Replace with the actual date
+    imageURL:
+      "/images/certificates/The_Complete_Data_Structures_and_Algorithms_Course_in_Java.png",
     skillsEarned: ["Data Structures", "Algorithms", "Java"],
-    tag: ["All", "Programming"],
+    tag: ["All", "Java"],
     viewLink: "",
   },
+  // Add more certificate objects following the same format for other certificates
 ];
