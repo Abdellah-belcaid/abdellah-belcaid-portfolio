09b512bcb141a3024845b596525fe199bf1ea023
belcaid <belcaid.abdellah2001@gmail.com>
2023-11-02 21:05:03 +0100

    update top left image

--- src/components/UI/TopLeftImg.js
+++ src/components/UI/TopLeftImg.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 const TopLeftImg = () => {
   return (
     <div className="absolute left-0 top-0 mix-blend-color-dodge animate-pulse z-10 w-[200px] xl:w-[300px] opacity-40 dark:opacity-50">
-      <Image src="/images/corner.svg" width={300} height={300} alt="" />
+      <Image src="/images/top-left-img-1.png" width={300} height={300} alt="" />
     </div>
   );
 };
